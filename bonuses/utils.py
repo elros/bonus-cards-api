43598243abf4aa8313b8abc7f377a983872416ec
Artem Maslovskiy <em.arcamenel@gmail.com>
2017-10-28 19:49:06 +0300

    add simple brute force protection

--- bonuses/utils.py
+++ bonuses/utils.py
@@ -1,8 +1,37 @@
-from flask import g, current_app
-from flask_pymongo import PyMongo
+import time
+from collections import Counter
+from functools import wraps
+
+from flask import request, current_app
 
 
-def get_mongo():
-    if not hasattr(g, 'mongo_connection'):
-        g.mongo_connection = PyMongo(current_app)
-    return g.mongo_connection
+def make_bruteforce_ip_filter(window_cfg, threshold_cfg, lag_cfg):
+
+    def decorator(func):
+
+        tracking_time = None
+        counter = None
+
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            nonlocal tracking_time, counter
+
+            window = current_app.config[window_cfg]
+            threshold = current_app.config[threshold_cfg]
+            lag = current_app.config[lag_cfg]
+
+            current_time = int(time.time()) // window * window
+            if current_time != tracking_time:
+                tracking_time = current_time
+                counter = Counter()
+
+            request_ip = request.remote_addr
+            counter[request_ip] += 1
+            if counter[request_ip] > threshold:
+                time.sleep(lag)
+
+            return func(*args, **kwargs)
+
+        return wrapper
+
+    return decorator

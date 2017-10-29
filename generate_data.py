6e6da204ddc988118bc3cfadf7509dc972d7c4ec
Artem Maslovskiy <em.arcamenel@gmail.com>
2017-10-29 22:37:03 +0300

    add random data generator

--- generate_data.py
+++ generate_data.py
@@ -1,4 +1,6 @@
 import os
+import random
+from datetime import datetime, timedelta
 
 
 os.environ['BONUSES_API_SETTINGS'] = os.path.join(os.path.dirname(__file__), 'settings.py')
@@ -6,9 +8,24 @@ os.environ['BONUSES_API_SETTINGS'] = os.path.join(os.path.dirname(__file__), 'se
 from bonuses.app import bonuses_app, bcrypt, mongo
 
 
+LOCATIONS = [
+    'KRR',
+    'DME',
+    'SVO',
+    'LED',
+]
+
+
 def main():
     with bonuses_app.app_context():
+        delete_objects()
         create_users()
+        create_random_transactions()
+
+
+def delete_objects():
+    mongo.db.users.delete_many({})
+    mongo.db.transactions.delete_many({})
 
 
 def create_users():
@@ -28,6 +45,23 @@ def create_users():
             'bonus_card_id': '000002',
         },
     ])
+
+
+def create_random_transactions(count=42):
+    users = list(mongo.db.users.find())
+    for _ in range(count):
+        create_transaction(user=random.choice(users))
+
+
+def create_transaction(user):
+    with bonuses_app.app_context():
+        mongo.db.transactions.insert_one({
+            'bonus_card_id': user['bonus_card_id'],
+            'points': random.randint(1, 100),
+            'flight_from': random.choice(LOCATIONS),
+            'flight_to': random.choice(LOCATIONS),
+            'flight_date': (datetime.now() + timedelta(days=random.randint(-30, 0)))
+        })
 
 
 if __name__ == '__main__':

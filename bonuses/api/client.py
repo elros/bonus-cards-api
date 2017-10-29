e5f28aca06fb3d524df02a868be6865c6d9290db
Artem Maslovskiy <em.arcamenel@gmail.com>
2017-10-29 22:37:14 +0300

    add simple transactions list API

--- bonuses/api/client.py
+++ bonuses/api/client.py
@@ -1,6 +1,8 @@
 from flask import Blueprint, jsonify, current_app
 from flask_jwt import jwt_required, current_identity
 
+from bonuses.database import get_transactions_by_bonus_card_id
+
 
 api = Blueprint('client_api', __name__)
 
@@ -8,14 +10,16 @@ api = Blueprint('client_api', __name__)
 @api.route('/profile')
 @jwt_required()
 def get_profile():
-    return {
+    return jsonify({
         'full_name': current_identity.full_name,
         'email': current_identity.email,
         'bonus_card_id': current_identity.bonus_card_id,
-    }
+    })
 
 
 @api.route('/bonus-transactions/')
 @jwt_required()
 def get_bonus_transactions_list():
-    return jsonify({'todo': True})
+    return jsonify({
+        'transactions': get_transactions_by_bonus_card_id(current_identity.bonus_card_id)
+    })
--- bonuses/database.py
+++ bonuses/database.py
@@ -1,6 +1,6 @@
 from collections import namedtuple
 
-from flask_pymongo import PyMongo
+from flask_pymongo import PyMongo, ObjectId
 
 
 mongo = PyMongo()
@@ -27,6 +27,13 @@ BonusTransaction = namedtuple('BonusTransaction', (
 ))
 
 
+def db_str(val):
+    if type(val) in (str, bytes):
+        return val
+    else:
+        return str(val)
+
+
 def get_user_by_username(username):
     user = mongo.db.users.find_one({'username': username})
     if user is None:
@@ -35,21 +42,35 @@ def get_user_by_username(username):
         return mongo_user_to_namedtuple(user)
 
 
-def get_user_by_id(_id):
-    user = mongo.db.users.find_one({'_id': _id})
+def mongo_user_to_namedtuple(user):
+    return User(
+        id=db_str(user['_id']),
+        username=db_str(user['username']),
+        pwd_hash=db_str(user['pwd_hash']),
+        full_name=db_str(user['full_name']),
+        email=db_str(user['email']),
+        bonus_card_id=db_str(user['bonus_card_id']),
+    )
+
+
+def get_user_by_id(user_id):
+    user = mongo.db.users.find_one({'_id': ObjectId(user_id)})
     if user is None:
         return None
     else:
         return mongo_user_to_namedtuple(user)
 
 
-def mongo_user_to_namedtuple(user):
-    convert = lambda val: val if type(val) in (str, bytes) else str(val)
-    return User(
-        id=convert(user['_id']),
-        username=convert(user['username']),
-        pwd_hash=convert(user['pwd_hash']),
-        full_name=convert(user['full_name']),
-        email=convert(user['email']),
-        bonus_card_id=convert(user['bonus_card_id']),
-    )
+def get_transactions_by_bonus_card_id(bonus_card_id):
+    transactions = mongo.db.transactions.find({'bonus_card_id': bonus_card_id})
+    return [
+        BonusTransaction(
+            id=db_str(transaction['_id']),
+            bonus_card_id=db_str(transaction['bonus_card_id']),
+            points=int(transaction['points']),
+            flight_from=db_str(transaction['flight_from']),
+            flight_to=db_str(transaction['flight_to']),
+            flight_date=db_str(transaction['flight_date']),
+        )
+        for transaction in transactions
+    ]

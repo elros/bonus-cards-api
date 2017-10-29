e13a226eace82bebcd18a0ab2cc4671a5ccc230b
Artem Maslovskiy <em.arcamenel@gmail.com>
2017-10-29 19:28:10 +0300

    remove unused 'login' method

--- bonuses/api/client.py
+++ bonuses/api/client.py
@@ -1,23 +1,8 @@
 from flask import Blueprint, jsonify, current_app
 from flask_jwt import jwt_required, current_identity
 
-from bonuses.utils import make_bruteforce_ip_filter
-
 
 api = Blueprint('client_api', __name__)
-
-
-bruteforce_filter = make_bruteforce_ip_filter(
-    window_cfg='BRUTEFORCE_WINDOW',
-    threshold_cfg='BRUTEFORCE_THRESHOLD',
-    lag_cfg='BRUTEFORCE_LAG'
-)
-
-
-@api.route('/login', methods=['POST'])
-@bruteforce_filter
-def make_login():
-    return jsonify({'todo': True})
 
 
 @api.route('/profile')
